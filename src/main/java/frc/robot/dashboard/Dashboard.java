a17316d45beeb356d90b6d4659f074cee8aa0990
Paradox0100 <milo.saucier@gmail.com4>
2024-10-16 19:10:51 -0400

    ok can maintain a connection with the elastic dashboard

--- src/main/java/frc/robot/dashboard/Dashboard.java
+++ src/main/java/frc/robot/dashboard/Dashboard.java
@@ -1,7 +1,27 @@
 package frc.robot.dashboard;
 
-public abstract class Dashboard {
-    public testSend() {
-        Shuffleboard.getTab("Numbers").add("Pi", 3.14)
-    }
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+
+public class Dashboard
+{
+	public void testSend()
+	{
+		Shuffleboard.getTab("Numbers").add("Pi", 3.14);
+		doomLoop();
+	}
+
+	// so it will remain connected
+	public void doomLoop()
+	{
+		while (true)
+		{
+			try
+			{
+				Thread.sleep(200);
+			} catch (InterruptedException e)
+			{
+				Thread.currentThread().interrupt();
+			}
+		}
+	}
 }
