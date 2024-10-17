ccf9f767685f55cc10414345739d9cbf3671a3db
Paradox0100 <milo.saucier@gmail.com4>
2024-10-16 20:14:01 -0400

    added ability for multiple data type upload

--- src/main/java/frc/robot/dashboard/Dashboard.java
+++ src/main/java/frc/robot/dashboard/Dashboard.java
@@ -6,8 +6,42 @@ public class Dashboard
 {
 	public void testSend()
 	{
-		Shuffleboard.getTab("Numbers").add("Pi", 3.14);
+		sendString("testing", "name", "Reefscape");
+		sendInt("testing", "everett IQ", 115);
+		sendFloat("testing", "pi", 3.14f);
+		sendDouble("testing", "e", 2.71);
+		sendBool("testing", "is everett zesty", true);
 		doomLoop();
+	}
+
+	public void sendString(String tab, String title, String value)
+	{
+		Shuffleboard.getTab(tab).add(title, value);
+	}
+
+	public void sendInt(String tab, String title, int value)
+	{
+		Shuffleboard.getTab(tab).add(title, value);
+	}
+
+	public void sendDouble(String tab, String title, double value)
+	{
+		Shuffleboard.getTab(tab).add(title, value);
+	}
+
+	public void sendFloat(String tab, String title, float value)
+	{
+		Shuffleboard.getTab(tab).add(title, value);
+	}
+
+	public void sendBool(String tab, String title, boolean value)
+	{
+		Shuffleboard.getTab(tab).add(title, value);
+	}
+
+	public void sendShort(String tab, String title, short value)
+	{
+		Shuffleboard.getTab(tab).add(title, value);
 	}
 
 	// so it will remain connected
