e314fd900d3088cc416db24e194b0fc41b755021
Paradox0100 <125032753+Paradox0100@users.noreply.github.com>
2024-12-11 19:57:44 -0500

    65 create container for zippy (#72)
    * made Zippy container and removed Crabby container
    
    * added Zippy stuff and removed Crabby stuff
    
    * built for formating
    
    * removed apt
    
    * added Zippy OI deleration stuff

---
+++ src/main/java/frc/robot/zippy/ZippyDriverOI.java
@@ -0,0 +1,11 @@
+package frc.robot.zippy;
+
+import frc.robot.robots.ZippyContainer;
+
+public class ZippyDriverOI implements ZippyOI
+{
+	@Override
+	public void bindOI(ZippyContainer container)
+	{
+	}
+}
