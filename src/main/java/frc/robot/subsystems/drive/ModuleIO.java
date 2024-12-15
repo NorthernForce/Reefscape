031f1da1e56209ad3f4c56f90fc4976d4727864e
EverettHThe1 <everett.hurder@gmail.com>
2024-12-15 09:33:18 -0500

    build

--- src/main/java/frc/robot/subsystems/drive/ModuleIO.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIO.java
@@ -11,7 +11,6 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 
-
 package frc.robot.subsystems.drive;
 
 import edu.wpi.first.math.geometry.Rotation2d;
