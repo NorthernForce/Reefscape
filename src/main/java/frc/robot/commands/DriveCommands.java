8deb889f92604ef23446b999920b72da19712db7
cml1010101 <cml1010101@gmail.com>
2024-12-17 18:58:28 -0500

    Fix joystickDrive

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -14,21 +14,16 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.drive.Drive;
+
 import java.util.function.DoubleSupplier;
+import java.util.function.DoubleUnaryOperator;
 
 public class DriveCommands
 {
-	private static final double DEADBAND = 0.1;
 
 	private DriveCommands()
 	{
@@ -45,32 +40,20 @@ public class DriveCommands
 	 * @return a runable command to drive el robo
 	 */
 	public static Command joystickDrive(Drive drive, DoubleSupplier xSupplier, DoubleSupplier ySupplier,
-			DoubleSupplier omegaSupplier)
+			DoubleSupplier omegaSupplier, double threshold)
 	{
 		return Commands.run(() ->
 		{
-			// Apply deadband
-			double linearMagnitude = MathUtil
-					.applyDeadband(Math.hypot(xSupplier.getAsDouble(), ySupplier.getAsDouble()), DEADBAND);
-			Rotation2d linearDirection = new Rotation2d(xSupplier.getAsDouble(), ySupplier.getAsDouble());
-			double omega = MathUtil.applyDeadband(omegaSupplier.getAsDouble(), DEADBAND);
-
-			// Square values
-			linearMagnitude = linearMagnitude * linearMagnitude;
-			omega = Math.copySign(omega * omega, omega);
-
-			// Calcaulate new linear velocity
-			Translation2d linearVelocity = new Pose2d(new Translation2d(), linearDirection)
-					.transformBy(new Transform2d(linearMagnitude, 0.0, new Rotation2d())).getTranslation();
-
-			// Convert to field relative speeds & send command
-			boolean isFlipped = DriverStation.getAlliance().isPresent()
-					&& DriverStation.getAlliance().get() == Alliance.Red;
-			drive.runVelocity(
-					ChassisSpeeds.fromFieldRelativeSpeeds(linearVelocity.getX() * drive.getMaxLinearSpeedMetersPerSec(),
-							linearVelocity.getY() * drive.getMaxLinearSpeedMetersPerSec(),
-							omega * drive.getMaxAngularSpeedRadPerSec(),
-							isFlipped ? drive.getRotation().plus(new Rotation2d(Math.PI)) : drive.getRotation()));
+			DoubleUnaryOperator process = v -> MathUtil.applyDeadband(v, threshold, 1.);
+			double x = process.applyAsDouble(xSupplier.getAsDouble());
+			double y = process.applyAsDouble(ySupplier.getAsDouble());
+			double omega = process.applyAsDouble(omegaSupplier.getAsDouble());
+			x *= Math.abs(x);
+			y *= Math.abs(y);
+			omega *= Math.abs(omega);
+			ChassisSpeeds speeds = new ChassisSpeeds(x, y, omega);
+			speeds.toRobotRelativeSpeeds(drive.getRotation());
+			drive.runVelocity(speeds);
 		}, drive);
 	}
 
