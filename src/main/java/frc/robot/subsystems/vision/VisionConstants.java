0b647809cc476f27d6290d2199ba14405f8c13a3
Whiteheatzack <whiteheatzack@gmail.com>
2024-12-18 17:47:24 -0500

    Format the freaking code

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -14,10 +14,14 @@ public class VisionConstants
 	public static String camera2Name = "camera_2";
 	public static String camera3Name = "camera_3";
 
-	public static Transform3d robotToCamera0 = new Transform3d(13.731, 13.731, 11.248, new Rotation3d(0, Math.toRadians(10), Math.toRadians(45)));
-	public static Transform3d robotToCamera1 = new Transform3d(13.731, -13.731, 11.248, new Rotation3d(0, Math.toRadians(10), Math.toRadians(135)));
-	public static Transform3d robotToCamera2 = new Transform3d(-13.731, -13.731, 11.248, new Rotation3d(0, Math.toRadians(10), Math.toRadians(225)));
-	public static Transform3d robotToCamera3 = new Transform3d(-13.731, 13.731, 11.248, new Rotation3d(0, Math.toRadians(10), Math.toRadians(315)));
+	public static Transform3d robotToCamera0 = new Transform3d(13.731, 13.731, 11.248,
+			new Rotation3d(0, Math.toRadians(10), Math.toRadians(45)));
+	public static Transform3d robotToCamera1 = new Transform3d(13.731, -13.731, 11.248,
+			new Rotation3d(0, Math.toRadians(10), Math.toRadians(135)));
+	public static Transform3d robotToCamera2 = new Transform3d(-13.731, -13.731, 11.248,
+			new Rotation3d(0, Math.toRadians(10), Math.toRadians(225)));
+	public static Transform3d robotToCamera3 = new Transform3d(-13.731, 13.731, 11.248,
+			new Rotation3d(0, Math.toRadians(10), Math.toRadians(315)));
 
 	public static double maxAmbiguity = 0.3;
 	public static double maxZError = 0.75;
