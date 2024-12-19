8db256a4f86a0488fd6de63b26a95a63fcab58e8
EverettHThe1 <everett.hurder@gmail.com>
2024-12-18 19:01:41 -0500

    added the constants to drive

--- src/main/java/frc/robot/util/DriveConstants.java
+++ src/main/java/frc/robot/util/DriveConstants.java
@@ -1,5 +1,7 @@
 package frc.robot.util;
 
 public record DriveConstants(double odometryFrequency, double maxLinearSpeed, double trackWidthX, double trackWidthY,
-		double driveBaseRadius, double maxAngularSpeed) {
+        double driveBaseRadius, double maxAngularSpeed, double mk4GearRatio, double kDriveGearRatio,
+        double kSteerGearRatio, double kWheelRadius, double kWheelCircumference, double kDriveP, double kTurnP,
+        double kMaxDriveSpeed) {
 }
