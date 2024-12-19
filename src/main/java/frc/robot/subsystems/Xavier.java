5dad9daba877c2e0b2f795fd2e285d45ca8ee44c
Kasey Nanovic <148725607+kcnan@users.noreply.github.com>
2024-12-18 19:00:58 -0500

    fixed formatting

--- src/main/java/frc/robot/subsystems/Xavier.java
+++ src/main/java/frc/robot/subsystems/Xavier.java
@@ -10,40 +10,46 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  */
 public class Xavier extends SubsystemBase
 {
-    protected final NetworkTableInstance instance;
-    protected final NetworkTable table;
-    protected final FloatSubscriber xRadSubscriber;
-    /**
-     * Create a new Xavier subsystem
-     * @param config the configuration for this subsystem
-     */
-    public Xavier(String tableName)
-    {
-        instance = NetworkTableInstance.getDefault();
-        table = instance.getTable(tableName);
-        xRadSubscriber = table.getFloatTopic("note_rad").subscribe(Float.NaN);
-    }
-    /**
-     * Gets the radian of the note the Xavier is most confident about
-     * @return x radian of note relative to center of robot (NaN if none detected)
-     */
-    public float getYawRadians()
-    {
-        return xRadSubscriber.get();
-    }
-    /**
-     * Gets whether the Xavier/Nano is connected or not
-     * @return whether Xavier/Nano is connected or not
-     */
-    public boolean isConnected()
-    {
-        for (var connection : instance.getConnections())
-        {
-            if (connection.remote_id.contains("skynet"))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
+	protected final NetworkTableInstance instance;
+	protected final NetworkTable table;
+	protected final FloatSubscriber xRadSubscriber;
+
+	/**
+	 * Create a new Xavier subsystem
+	 * 
+	 * @param config the configuration for this subsystem
+	 */
+	public Xavier(String tableName)
+	{
+		instance = NetworkTableInstance.getDefault();
+		table = instance.getTable(tableName);
+		xRadSubscriber = table.getFloatTopic("note_rad").subscribe(Float.NaN);
+	}
+
+	/**
+	 * Gets the radian of the note the Xavier is most confident about
+	 * 
+	 * @return x radian of note relative to center of robot (NaN if none detected)
+	 */
+	public float getYawRadians()
+	{
+		return xRadSubscriber.get();
+	}
+
+	/**
+	 * Gets whether the Xavier/Nano is connected or not
+	 * 
+	 * @return whether Xavier/Nano is connected or not
+	 */
+	public boolean isConnected()
+	{
+		for (var connection : instance.getConnections())
+		{
+			if (connection.remote_id.contains("skynet"))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 }
--- src/main/java/frc/robot/subsystems/usablename/UsableName.java
+++ src/main/java/frc/robot/subsystems/usablename/UsableName.java
@@ -5,22 +5,25 @@ import frc.robot.subsystems.Xavier;
 
 public class UsableName extends SubsystemBase implements UsableNameIO
 {
-    private final Xavier xavier;
-    public UsableName(Xavier xavier)
-    {
-        this.xavier = xavier;
-    }
-    public UsableNameIODataAutoLogged getVisionData()
-    {
-        float yaw = xavier.getYawRadians();
-        var data = new UsableNameIODataAutoLogged();
-        data.connected = xavier.isConnected();
-        data.detected = yaw == Float.NaN;
-        data.yawRadians = yaw;
-        return data;
-    }
-    public boolean isConnected()
-    {
-        return xavier.isConnected();
-    }
+	private final Xavier xavier;
+
+	public UsableName(Xavier xavier)
+	{
+		this.xavier = xavier;
+	}
+
+	public UsableNameIODataAutoLogged getVisionData()
+	{
+		float yaw = xavier.getYawRadians();
+		var data = new UsableNameIODataAutoLogged();
+		data.connected = xavier.isConnected();
+		data.detected = yaw == Float.NaN;
+		data.yawRadians = yaw;
+		return data;
+	}
+
+	public boolean isConnected()
+	{
+		return xavier.isConnected();
+	}
 }
