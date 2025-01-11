552a27e1bf7444398c46304b597e0b34cb3297e1
Paradox0100 <milo.saucier@gmail.com>
2025-01-11 13:10:43 -0500

    Merge remote-tracking branch 'origin/devel' into 26-write-code-for-ctre-candle-led-subsystem-1

---
+++ src/main/java/frc/robot/zippy/oi/ZippyDriverOI.java
@@ -0,0 +1,11 @@
+package frc.robot.zippy.oi;
+
+import frc.robot.zippy.ZippyContainer;
+
+public class ZippyDriverOI implements ZippyOI
+{
+	@Override
+	public void bindOI(ZippyContainer container)
+	{
+	}
+}
--- src/main/java/org/northernforce/util/NFRRobotContainer.java
+++ src/main/java/org/northernforce/util/NFRRobotContainer.java
@@ -1,9 +1,6 @@
 package org.northernforce.util;
 
-import edu.wpi.first.math.Pair;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import java.util.Map;
 
 /**
  * A robot container should contain all subsystems and states. Has various
@@ -11,9 +8,7 @@ import java.util.Map;
  */
 public interface NFRRobotContainer
 {
-	/**
-	 * Binds the commands from subsystems to the operator interfaces.
-	 */
+	/** Binds the commands from subsystems to the operator interfaces. */
 	public void bindOI();
 
 	/** Runs periodically (every 20 ms) regardless of mode. */
@@ -31,27 +26,15 @@ public interface NFRRobotContainer
 	{
 	}
 
-	/**
-	 * Returns a list of autonomous commands. First is the default command.
-	 *
-	 * @return all of the autonomous options
-	 */
-	public Map<String, Command> getAutonomousOptions();
+	/** Runs at the start of autonomous */
+	public default void autonomousInit()
+	{
+	}
 
 	/**
-	 * Returns a list of the starting locations. First is the default starting
-	 * location
-	 *
-	 * @return all of the starting locations
+	 * Get the selected autonomous command
+	 * 
+	 * @return the selected autonomous command
 	 */
-	public Map<String, Pose2d> getStartingLocations();
-
-	/**
-	 * Gets the default autonomous option in case none is selected.
-	 *
-	 * @return the default autonomous.
-	 */
-	public Pair<String, Command> getDefaultAutonomous();
-
-	public void setInitialPose(Pose2d pose);
+	public Command getAutonomousCommand();
 }
