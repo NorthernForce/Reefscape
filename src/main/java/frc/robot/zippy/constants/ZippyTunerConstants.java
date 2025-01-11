aafe75c71b06428e49499a5baebba1356cc928d4
EverettHThe1 <everett.hurder@gmail.com>
2025-01-10 19:56:14 -0500

    silly things

--- src/main/java/frc/robot/zippy/constants/ZippyTunerConstants.java
+++ src/main/java/frc/robot/zippy/constants/ZippyTunerConstants.java
@@ -60,11 +60,11 @@ public class ZippyTunerConstants
 					.withStatorCurrentLimit(Amps.of(60)).withStatorCurrentLimitEnable(true));
 	private static final CANcoderConfiguration encoderInitialConfigs = new CANcoderConfiguration();
 	// Configs for the Pigeon 2; leave this null to skip applying Pigeon 2 configs
-	private static final Pigeon2Configuration pigeonConfigs = null;
+	private static final Pigeon2Configuration pigeonConfigs = new Pigeon2Configuration();
 
 	// CAN bus that the devices are located on;
 	// All swerve devices must share the same CAN bus
-	public static final CANBus kCANBus = new CANBus("", "./logs/example.hoot");
+	public static final CANBus kCANBus = new CANBus("drive", "./logs/example.hoot");
 
 	// Theoretical free speed (m/s) at 12 V applied output;
 	// This needs to be tuned to your individual robot
