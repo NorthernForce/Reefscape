23c051ae8ddc58d35bdab2ae9be3af8596d0ddd4
EverettHThe1 <139522291+Daboss-1@users.noreply.github.com>
2025-01-14 19:55:46 +0000

    add comments

--- src/main/java/frc/robot/subsystems/pneumatic/PneumaticHub.java
+++ src/main/java/frc/robot/subsystems/pneumatic/PneumaticHub.java
@@ -6,32 +6,49 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PneumaticHub extends SubsystemBase
 {
+
 	private final PneumaticHubIO m_io;
 	private final PneumaticHubIOInputsAutoLogged m_inputs = new PneumaticHubIOInputsAutoLogged();
 
+    /**
+     * Initialize new PneumaticHub
+     * @param io the io instance to pass in
+     */
 	public PneumaticHub(PneumaticHubIO io)
 	{
 		m_io = io;
 	}
 
+    /**
+     * toggles whether the pneumatic hub hardware interface is enabled or disabled
+     */
 	public void toggle()
 	{
 		m_io.toggle();
 	}
 
+    /**
+     * enables the pneumatic hub io hardware interface
+     */
 	public void enable()
 	{
 		m_io.enable();
 	}
 
+    /**
+     * disables the pneumatic hub io hardware interface
+     */
 	public void disable()
 	{
 		m_io.disable();
 	}
 
+    /**
+     * periodic input update and processing
+     */
 	public void periodic()
 	{
 		m_io.updateInputs(m_inputs);
-		Logger.processInputs("Pneumatics/PneumaticHub", m_inputs);
+		Logger.processInputs(m_io.getName(), m_inputs);
 	}
 }
--- src/main/java/frc/robot/subsystems/pneumatic/PneumaticHubIO.java
+++ src/main/java/frc/robot/subsystems/pneumatic/PneumaticHubIO.java
@@ -11,18 +11,38 @@ public interface PneumaticHubIO
 		public boolean isAtPressure;
 	}
 
+    /**
+     * toggle functionality interface 
+     */
 	public default void toggle()
 	{
 	}
 
+    /**
+     * enable interface functionality
+     */
 	public default void enable()
 	{
 	}
 
+
+    /**
+     * enable interface functionality
+     */
 	public default void disable()
 	{
 	}
+ 
 
+    /**
+     * get the hardware interface name (even though we ONLY USE REV CONNOR (😡))
+     */
+    public String getName();
+
+    /**
+     * update inputs for the Hub
+     * @param inputs inputs to update with periodic
+     */
 	public default void updateInputs(PneumaticHubIOInputs inputs)
 	{
 	}
