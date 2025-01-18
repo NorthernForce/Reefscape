622361da0b3f71d4847be730da33f9edb6ad4bda
EverettHThe1 <everett.hurder@gmail.com>
2025-01-18 16:03:47 -0500

    add empty ImGui class

---
+++ src/main/java/frc/robot/subsystems/dashboard/ZippyDashboardIOIMGUI.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems.dashboard;
+
+public class ZippyDashboardIOIMGUI implements ZippyDashboardIO
+{
+
+	public void publishValue(String key, Object value)
+	{
+
+	}
+
+	public void updateInputs(ZippyDashboardIOInputs inputs)
+	{
+
+	}
+
+}
