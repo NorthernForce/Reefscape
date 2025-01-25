8e8a7f567161858662843daf1430b52df0623a1b
EverettHThe1 <everett.hurder@gmail.com>
2025-01-25 18:41:31 -0500

    add ultrasonic sensor

--- src/main/java/frc/robot/subsystems/rollers/Rollers.java
+++ src/main/java/frc/robot/subsystems/rollers/Rollers.java
@@ -9,30 +9,33 @@ import frc.robot.subsystems.rollers.sensor.SensorIO;
 
 public class Rollers implements Subsystem
 {
-	public RollersIO intakeIO;
-	public SensorIO sensorIO;
+	public RollersIO m_intakeIO;
+	public SensorIO m_sensorIOAlgae;
+	public SensorIO m_sensorIOCoral;
 	private IntakeIOInputsAutoLogged m_inputs = new IntakeIOInputsAutoLogged();
-	private SensorIOInputsAutoLogged m_beamBreakInputs = new SensorIOInputsAutoLogged();
+	private SensorIOInputsAutoLogged m_sensorIOAlgaeInputs = new SensorIOInputsAutoLogged();
+	private SensorIOInputsAutoLogged m_sensorIOCoralInputs = new SensorIOInputsAutoLogged();
 
-	public Rollers(RollersIO intakeIO, SensorIO sensorIO)
+	public Rollers(RollersIO intakeIO, SensorIO sensorIOAlgae, SensorIO sensorIOCoral)
 	{
-		this.intakeIO = intakeIO;
-		this.sensorIO = sensorIO;
+		m_intakeIO = intakeIO;
+		m_sensorIOAlgae = sensorIOAlgae;
+		m_sensorIOCoral = sensorIOCoral;
 	}
 
 	public void intake()
 	{
-		intakeIO.set(1);
+		m_intakeIO.set(1);
 	}
 
 	public void outtake()
 	{
-		intakeIO.set(-1);
+		m_intakeIO.set(-1);
 	}
 
 	public void stop()
 	{
-		intakeIO.set(0);
+		m_intakeIO.set(0);
 	}
 
 	public Command getIntakeCommand()
@@ -48,7 +51,7 @@ public class Rollers implements Subsystem
 			@Override
 			public boolean isFinished()
 			{
-				return m_beamBreakInputs.hasPiece;
+				return m_sensorIOAlgaeInputs.hasPiece || m_sensorIOCoralInputs.hasPiece;
 			}
 
 			@Override
@@ -61,9 +64,11 @@ public class Rollers implements Subsystem
 
 	public void periodic()
 	{
-		intakeIO.updateInputs(m_inputs);
-		sensorIO.updateInputs(m_beamBreakInputs);
+		m_intakeIO.updateInputs(m_inputs);
+		m_sensorIOAlgae.updateInputs(m_sensorIOAlgaeInputs);
+		m_sensorIOCoral.updateInputs(m_sensorIOCoralInputs);
 		Logger.processInputs(getName() + "/Motor", m_inputs);
-		Logger.processInputs(getName() + "/BeamBreak", m_beamBreakInputs);
+		Logger.processInputs(getName() + "/sensors/algae", m_sensorIOAlgaeInputs);
+		Logger.processInputs(getName() + "/sensors/coral", m_sensorIOCoralInputs);
 	}
 }
---
+++ src/main/java/frc/robot/subsystems/rollers/sensor/SensorIOUltrasonic.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems.rollers.sensor;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+
+
+public class SensorIOUltrasonic implements SensorIO{
+
+    private final AnalogInput ultrasonicSensor;
+    private final double distanceToStopAt;
+
+    public SensorIOUltrasonic(int signalChannel, double distanceToStopAt) {
+        ultrasonicSensor = new AnalogInput(signalChannel);
+        this.distanceToStopAt = distanceToStopAt;
+    }
+
+    /**
+     * gets the distance from the ultrasonic sensor
+     * @return the distance, where 0V = 20 mm, and 5V = 4000 mm. Do the math.
+     */
+    private double getDistance() {
+        return ultrasonicSensor.getVoltage() / 5.0 * 3980.0 + 20.0;
+    }
+    @Override
+    public void updateInputs(SensorIOInputs inputs)
+    {
+        inputs.hasPiece = getDistance() <= distanceToStopAt;
+    }
+
+}
