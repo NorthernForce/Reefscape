7fe1a63a694ee3f743a4d1182116eac375880298
EverettHThe1 <everett.hurder@gmail.com>
2025-01-25 18:42:34 -0500

    build

--- src/main/java/frc/robot/subsystems/rollers/sensor/SensorIOUltrasonic.java
+++ src/main/java/frc/robot/subsystems/rollers/sensor/SensorIOUltrasonic.java
@@ -2,28 +2,32 @@ package frc.robot.subsystems.rollers.sensor;
 
 import edu.wpi.first.wpilibj.AnalogInput;
 
+public class SensorIOUltrasonic implements SensorIO
+{
 
-public class SensorIOUltrasonic implements SensorIO{
+	private final AnalogInput ultrasonicSensor;
+	private final double distanceToStopAt;
 
-    private final AnalogInput ultrasonicSensor;
-    private final double distanceToStopAt;
+	public SensorIOUltrasonic(int signalChannel, double distanceToStopAt)
+	{
+		ultrasonicSensor = new AnalogInput(signalChannel);
+		this.distanceToStopAt = distanceToStopAt;
+	}
 
-    public SensorIOUltrasonic(int signalChannel, double distanceToStopAt) {
-        ultrasonicSensor = new AnalogInput(signalChannel);
-        this.distanceToStopAt = distanceToStopAt;
-    }
+	/**
+	 * gets the distance from the ultrasonic sensor
+	 * 
+	 * @return the distance, where 0V = 20 mm, and 5V = 4000 mm. Do the math.
+	 */
+	private double getDistance()
+	{
+		return ultrasonicSensor.getVoltage() / 5.0 * 3980.0 + 20.0;
+	}
 
-    /**
-     * gets the distance from the ultrasonic sensor
-     * @return the distance, where 0V = 20 mm, and 5V = 4000 mm. Do the math.
-     */
-    private double getDistance() {
-        return ultrasonicSensor.getVoltage() / 5.0 * 3980.0 + 20.0;
-    }
-    @Override
-    public void updateInputs(SensorIOInputs inputs)
-    {
-        inputs.hasPiece = getDistance() <= distanceToStopAt;
-    }
+	@Override
+	public void updateInputs(SensorIOInputs inputs)
+	{
+		inputs.hasPiece = getDistance() <= distanceToStopAt;
+	}
 
 }
