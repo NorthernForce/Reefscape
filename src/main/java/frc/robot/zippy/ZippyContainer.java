3221b72c3e8b68217d609f5c215a6ce797548677
Kasey Nanovic <148725607+kcnan@users.noreply.github.com>
2025-01-29 18:33:45 -0500

    one step at a time amiright

--- src/main/java/frc/robot/zippy/ZippyContainer.java
+++ src/main/java/frc/robot/zippy/ZippyContainer.java
@@ -3,20 +3,25 @@ package frc.robot.zippy;
 import java.util.Map;
 import java.util.function.Supplier;
 
-import javax.xml.stream.FactoryConfigurationError;
-
 import org.northernforce.util.NFRRobotContainer;
+
+import com.pathplanner.lib.auto.NamedCommands;
 
 import choreo.auto.AutoChooser;
 import choreo.auto.AutoFactory;
 import choreo.auto.AutoRoutine;
 import choreo.trajectory.SwerveSample;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import frc.robot.Constants;
 import frc.robot.FieldConstants;
 import frc.robot.subsystems.phoenix6.PhoenixCommandDrive;
@@ -32,6 +37,9 @@ public class ZippyContainer implements NFRRobotContainer
 	private final Alliance alliance = DriverStation.getAlliance().orElse(Alliance.Red);
 	private AutoFactory factory = null;
 	private AutoChooser autoChooser = null;
+	private AutoRoutine hi = null;
+	private Command hicmd = null;
+	Field2d field;
 
 	public ZippyContainer()
 	{
@@ -40,6 +48,9 @@ public class ZippyContainer implements NFRRobotContainer
 				ZippyTunerConstants.FrontLeft, ZippyTunerConstants.FrontRight, ZippyTunerConstants.BackLeft,
 				ZippyTunerConstants.BackRight);
 		drive.setOperatorPerspectiveForward(FieldConstants.getFieldRotation(alliance));
+		field = new Field2d();
+		drive.resetPose(new Pose2d(7, 7, new Rotation2d(0)));
+		Shuffleboard.getTab("General").add("pose2d", field);
 	}
 
 	public PhoenixCommandDrive getDrive()
@@ -71,6 +82,7 @@ public class ZippyContainer implements NFRRobotContainer
 		{
 			drive.setOperatorPerspectiveForward(FieldConstants.getFieldRotation(alliance));
 		}
+		field.setRobotPose(drive.getPose());
 	}
 
 	@Override
@@ -87,10 +99,28 @@ public class ZippyContainer implements NFRRobotContainer
 					sample.vy + ZippyConstants.AutoConstants.yPID.calculate(robot.getY(), sample.y),
 					sample.omega + ZippyConstants.AutoConstants.rPID.calculate(robot.getRotation().getRadians(),
 							sample.heading));
+			System.out.println("AUTO STATS:");
+			System.out.println("x: " + speeds.vxMetersPerSecond);
+			System.out.println("y: " + speeds.vyMetersPerSecond);
+			System.out.println("r: " + speeds.omegaRadiansPerSecond);
 			drive.runVelocity(speeds);
 		}, true, drive);
-		factory.bind("test", Commands.run(() -> System.out.println("test binding ran!"))).bind("test2",
-				Commands.run(() -> System.out.println("yup yup!")));
+
+		NamedCommands.registerCommand("test", Commands.runOnce(() -> System.out.println("it works!")));
+		var routine = factory.newRoutine("test1");
+		var traj = routine.trajectory("testPath");
+		routine.active().onTrue(Commands.sequence(traj.resetOdometry(), traj.cmd()));
+		hi = routine;
+		// hicmd = factory.trajectoryCmd("testPath");
+		// RobotModeTriggers.autonomous().whileTrue(hicmd).whileTrue(Commands.run(() ->
+		// System.out.println("")));
+	}
+
+	@Override
+	public void autonomousPeriodic()
+	{
+		System.out.println("running auto...");
+		hi.poll();
 	}
 
 	public Map<String, Supplier<AutoRoutine>> getAutonomousCommands()
