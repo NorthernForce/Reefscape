fa7aa75813bc341b5db80a9921df28d1de0dec94
cml1010101 <115354809+cml1010101@users.noreply.github.com>
2025-02-13 20:32:02 -0500

    Feature/reset drive encoders (#171)
    * Add functionality to reset drive encoders
    
    * Almost there. Need to update dash now
    
    * Add button for resetting swerve
    
    * Make swerve reset happen when disabled

--- src/main/java/frc/robot/blenny/constants/BlennyConstants.java
+++ src/main/java/frc/robot/blenny/constants/BlennyConstants.java
@@ -6,6 +6,7 @@ import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.LinearAcceleration;
 import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.wpilibj.Preferences;
 import frc.robot.subsystems.superstructure.Superstructure.GenericSuperstructureGoal;
 import frc.robot.subsystems.superstructure.elevator.ElevatorIOTalonFX.ElevatorConstants;
 import frc.robot.subsystems.superstructure.wrist.WristIOTalonFX.WristConstants;
@@ -21,6 +22,14 @@ public class BlennyConstants
         public static final AngularVelocity MAX_ANGULAR_SPEED = RotationsPerSecond.of(0.7);
         public static final LinearAcceleration MAX_ACCELERATION = MetersPerSecondPerSecond.of(3.0);
         public static final AngularAcceleration MAX_ANGULAR_ACCELERATION = RotationsPerSecondPerSecond.of(0.7);
+        public static final Angle[] SWERVE_MODULE_OFFSETS =
+        { Rotations.of(Preferences.getDouble("kSwerveOffestFrontLeft", BlennyTunerConstants.FrontLeft.EncoderOffset)),
+                Rotations.of(Preferences.getDouble("kSwerveOffestFrontRight",
+                        BlennyTunerConstants.FrontRight.EncoderOffset)),
+                Rotations.of(
+                        Preferences.getDouble("kSwerveOffestBackLeft", BlennyTunerConstants.BackLeft.EncoderOffset)),
+                Rotations.of(Preferences.getDouble("kSwerveOffestBackRight",
+                        BlennyTunerConstants.BackRight.EncoderOffset)) };
     }
 
     public static class InnerElevatorConstants
