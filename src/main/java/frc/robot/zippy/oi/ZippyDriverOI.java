9a88a321a65e82bc406a7c3029fcdcd459b5235f
Paradox0100 <milo.saucier@gmail.com>
2025-02-22 13:15:51 -0500

    fixed merge error

--- src/main/java/frc/robot/zippy/oi/ZippyDriverOI.java
+++ src/main/java/frc/robot/zippy/oi/ZippyDriverOI.java
@@ -52,6 +52,7 @@ public class ZippyDriverOI implements ZippyOI
                 container.getDrive()));
         driverJoystick.b()
                 .whileTrue(Commands.sequence(
+                        Commands.runOnce(() -> SignalLogger.start()),
                         container.getDrive().getSysIdTranslationQuasistatic(SysIdRoutine.Direction.kForward),
                         container.getDrive().getSysIdTranslationQuasistatic(SysIdRoutine.Direction.kReverse),
                         container.getDrive().getSysIdTranslationDynamic(SysIdRoutine.Direction.kForward),
@@ -65,42 +66,9 @@ public class ZippyDriverOI implements ZippyOI
                         container.getDrive().getSysIdSteerQuasistatic(SysIdRoutine.Direction.kForward),
                         container.getDrive().getSysIdSteerQuasistatic(SysIdRoutine.Direction.kReverse),
                         container.getDrive().getSysIdSteerDynamic(SysIdRoutine.Direction.kForward),
-                        container.getDrive().getSysIdSteerDynamic(SysIdRoutine.Direction.kReverse)));
+                        container.getDrive().getSysIdSteerDynamic(SysIdRoutine.Direction.kReverse),
+                        Commands.runOnce(() -> SignalLogger.stop())));
 
-        driverJoystick.b()
-                .whileTrue(Commands.sequence(
-                        container.getDrive().getSysIdTranslationQuasistatic(SysIdRoutine.Direction.kForward),
-                        container.getDrive().getSysIdTranslationQuasistatic(SysIdRoutine.Direction.kReverse),
-                        container.getDrive().getSysIdTranslationDynamic(SysIdRoutine.Direction.kForward),
-                        container.getDrive().getSysIdTranslationDynamic(SysIdRoutine.Direction.kReverse),
-
-                        container.getDrive().getSysIdRotationQuasistatic(SysIdRoutine.Direction.kForward),
-                        container.getDrive().getSysIdRotationQuasistatic(SysIdRoutine.Direction.kReverse),
-                        container.getDrive().getSysIdRotationDynamic(SysIdRoutine.Direction.kForward),
-                        container.getDrive().getSysIdRotationDynamic(SysIdRoutine.Direction.kReverse),
-
-                        container.getDrive().getSysIdSteerQuasistatic(SysIdRoutine.Direction.kForward),
-                        container.getDrive().getSysIdSteerQuasistatic(SysIdRoutine.Direction.kReverse),
-                        container.getDrive().getSysIdSteerDynamic(SysIdRoutine.Direction.kForward),
-                        container.getDrive().getSysIdSteerDynamic(SysIdRoutine.Direction.kReverse)));
-
-        driverJoystick.b().whileTrue(Commands.sequence(Commands.runOnce(() -> SignalLogger.start()),
-
-                container.getDrive().getSysIdTranslationQuasistatic(SysIdRoutine.Direction.kForward),
-                container.getDrive().getSysIdTranslationQuasistatic(SysIdRoutine.Direction.kReverse),
-                container.getDrive().getSysIdTranslationDynamic(SysIdRoutine.Direction.kForward),
-                container.getDrive().getSysIdTranslationDynamic(SysIdRoutine.Direction.kReverse),
-
-                container.getDrive().getSysIdRotationQuasistatic(SysIdRoutine.Direction.kForward),
-                container.getDrive().getSysIdRotationQuasistatic(SysIdRoutine.Direction.kReverse),
-                container.getDrive().getSysIdRotationDynamic(SysIdRoutine.Direction.kForward),
-                container.getDrive().getSysIdRotationDynamic(SysIdRoutine.Direction.kReverse),
-
-                container.getDrive().getSysIdSteerQuasistatic(SysIdRoutine.Direction.kForward),
-                container.getDrive().getSysIdSteerQuasistatic(SysIdRoutine.Direction.kReverse),
-                container.getDrive().getSysIdSteerDynamic(SysIdRoutine.Direction.kForward),
-                container.getDrive().getSysIdSteerDynamic(SysIdRoutine.Direction.kReverse),
-                Commands.runOnce(() -> SignalLogger.stop())));
 
         driverJoystick.start().onTrue(Commands.runOnce(() -> container.getDrive().resetPose(FieldConstants
                 .convertPoseByAlliance(FieldConstants.ReefPositions.AB_ALGAE, FieldConstants.getAlliance()))));
