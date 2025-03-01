d52a71622000e85c6db8b62ae84e697828c3d2e6
cml1010101 <cml1010101@gmail.com>
2025-03-01 16:14:30 -0500

    Update at 'Sat Mar 01 16:14:30 EST 2025'

--- src/main/java/frc/robot/sebastian/oi/SebastianDriverOI.java
+++ src/main/java/frc/robot/sebastian/oi/SebastianDriverOI.java
@@ -138,7 +138,7 @@ public class SebastianDriverOI implements SebastianOI
         manipulatorController.povDown()
                 .whileTrue(container.getSuperstructure().getGoToGoalCommand(SebastianConstants.SuperstructureGoal.L4));
         manipulatorController.a().whileTrue(
-                container.getSuperstructure().getGoToGoalCommand(SebastianConstants.SuperstructureGoal.CORAL_STATION).andThen(container..));
+                container.getSuperstructure().getGoToGoalCommand(SebastianConstants.SuperstructureGoal.CORAL_STATION_PRE).andThen(container.getSuperstructure().getGoToGoalCommand(SebastianConstants.SuperstructureGoal.CORAL_STATION)));
         manipulatorController.b().whileTrue(container.getSuperstructure()
                 .getGoToGoalCommand(SebastianConstants.SuperstructureGoal.PROCESSOR_STATION));
         manipulatorController.y().whileTrue(
