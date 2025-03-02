b01fc7565533a904d9fba1f3ebd2d08799d41f4d
cml1010101 <cml1010101@gmail.com>
2025-03-02 10:08:37 -0500

    >

--- src/main/java/frc/robot/sebastian/SebastianContainer.java
+++ src/main/java/frc/robot/sebastian/SebastianContainer.java
@@ -267,9 +267,8 @@ public class SebastianContainer implements NFRRobotContainer
     public Command getCenter_H4Command()
     {
         return new SequentialCommandGroup(drive.getBackupCommand(0.5, -0.5),
-            superstructure.getGoToGoalCommand(SuperstructureGoal.L4),
-            drive.getFollowPathCommand("Center_H4"),
-            rollers.getOuttakeCoralCommand());
+                superstructure.getGoToGoalCommand(SuperstructureGoal.L4), drive.getFollowPathCommand("Center_H4"),
+                rollers.getOuttakeCoralCommand());
     }
 
     public Command getBlueMid_J4Command()
