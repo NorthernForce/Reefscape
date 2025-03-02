241c6b23d9bd07b25e06c8af0fab72ace453fd14
cml1010101 <cml1010101@gmail.com>
2025-03-02 10:10:55 -0500

    Update at 'Sun Mar 02 10:10:54 EST 2025'

--- src/main/java/frc/robot/sebastian/oi/SebastianDriverOI.java
+++ src/main/java/frc/robot/sebastian/oi/SebastianDriverOI.java
@@ -137,14 +137,15 @@ public class SebastianDriverOI implements SebastianOI
                 .whileTrue(container.getSuperstructure().getGoToGoalCommand(SebastianConstants.SuperstructureGoal.L3));
         manipulatorController.povDown()
                 .whileTrue(container.getSuperstructure().getGoToGoalCommand(SebastianConstants.SuperstructureGoal.L4));
-        manipulatorController.a().onTrue(Commands.either(
-                container.getSuperstructure()
+        manipulatorController.a().onTrue(Commands
+                .either(container.getSuperstructure()
                         .getGoToGoalCommand(SebastianConstants.SuperstructureGoal.CORAL_STATION_PRE)
                         .andThen(container.getSuperstructure().getHomingCommand(0.5, 0.5))
                         .andThen(container.getSuperstructure()
                                 .getGoToGoalCommand(SebastianConstants.SuperstructureGoal.CORAL_STATION)),
-                Commands.none(), () -> container.getSuperstructure().getGoal() != SuperstructureGoal.CORAL_STATION)
-                    .withTimeout(1.5));
+                        Commands.none(),
+                        () -> container.getSuperstructure().getGoal() != SuperstructureGoal.CORAL_STATION)
+                .withTimeout(1.5));
         manipulatorController.b().whileTrue(container.getSuperstructure()
                 .getGoToGoalCommand(SebastianConstants.SuperstructureGoal.PROCESSOR_STATION));
         manipulatorController.y().whileTrue(
