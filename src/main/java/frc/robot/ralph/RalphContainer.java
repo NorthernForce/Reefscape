103f9b9e962c04b88a0589a0044ab1db5cd9870b
EverettHThe1 <everett.hurder@gmail.com>
2025-03-04 16:00:41 -0500

    build

--- src/main/java/frc/robot/ralph/RalphContainer.java
+++ src/main/java/frc/robot/ralph/RalphContainer.java
@@ -159,28 +159,18 @@ public class RalphContainer implements NFRRobotContainer
 
     public Command getGoToReefPoseCommandLeft()
     {
-        return Commands.defer(
-                () -> getDrive()
-                        .driveToPose((getDashboard().getTargetPoseLeft(getDrive().getPose())),
-                                RalphConstants.PathplannerConstants.MAX_VELOCITY,
-                                RalphConstants.PathplannerConstants.MAX_ACCELERATION,
-                                RalphConstants.PathplannerConstants.MAX_ANGULAR_VELOCITY,
-                                RalphConstants.PathplannerConstants.MAX_ANGULAR_ACCELERATION)
-                        ,
-                Set.of());
+        return Commands.defer(() -> getDrive().driveToPose((getDashboard().getTargetPoseLeft(getDrive().getPose())),
+                RalphConstants.PathplannerConstants.MAX_VELOCITY, RalphConstants.PathplannerConstants.MAX_ACCELERATION,
+                RalphConstants.PathplannerConstants.MAX_ANGULAR_VELOCITY,
+                RalphConstants.PathplannerConstants.MAX_ANGULAR_ACCELERATION), Set.of());
     }
 
     public Command getGoToReefPoseCommandRight()
     {
-        return Commands.defer(
-                () -> getDrive()
-                        .driveToPose((getDashboard().getTargetPoseRight(getDrive().getPose())),
-                                RalphConstants.PathplannerConstants.MAX_VELOCITY,
-                                RalphConstants.PathplannerConstants.MAX_ACCELERATION,
-                                RalphConstants.PathplannerConstants.MAX_ANGULAR_VELOCITY,
-                                RalphConstants.PathplannerConstants.MAX_ANGULAR_ACCELERATION)
-                        ,
-                Set.of());
+        return Commands.defer(() -> getDrive().driveToPose((getDashboard().getTargetPoseRight(getDrive().getPose())),
+                RalphConstants.PathplannerConstants.MAX_VELOCITY, RalphConstants.PathplannerConstants.MAX_ACCELERATION,
+                RalphConstants.PathplannerConstants.MAX_ANGULAR_VELOCITY,
+                RalphConstants.PathplannerConstants.MAX_ANGULAR_ACCELERATION), Set.of());
     }
 
     public Command getGoToReefPoseCommandCenter()
