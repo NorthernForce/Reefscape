a74c3d34f1b3d43ef042ece8f9d578e14538c086
Whiteheatzack <whiteheatzack@gmail.com>
2025-03-04 15:41:31 -0500

    Fix names

--- src/main/java/frc/robot/ralph/RalphAutos.java
+++ src/main/java/frc/robot/ralph/RalphAutos.java
@@ -9,22 +9,26 @@ public class RalphAutos
 {
     public static void addAutoRoutines(RalphContainer container)
     {
-        container.getDashboard().addAutoRoutine("S1.LEAVE", new PathPlannerAuto("S1.LEAVE"));
-        container.getDashboard().addAutoRoutine("S1.PLACE", new PathPlannerAuto("S1.PLACE"));
-        container.getDashboard().addAutoRoutine("S2.LEAVE", new PathPlannerAuto("S2.LEAVE"));
-        container.getDashboard().addAutoRoutine("S2.PLACE", new PathPlannerAuto("S2.PLACE"));
-        container.getDashboard().addAutoRoutine("S3.LEAVE", new PathPlannerAuto("S3.LEAVE"));
-        container.getDashboard().addAutoRoutine("S3.PLACE", new PathPlannerAuto("S3.PLACE"));
+        container.getDashboard().addAutoRoutine("BLUE.LEAVE", new PathPlannerAuto("BLUE.LEAVE"));
+        container.getDashboard().addAutoRoutine("BLUE.PLACE.I", new PathPlannerAuto("BLUE.PLACE.I"));
+        container.getDashboard().addAutoRoutine("CENTER.LEAVE", new PathPlannerAuto("CENTER.LEAVE"));
+        container.getDashboard().addAutoRoutine("CENTER.PLACE.G", new PathPlannerAuto("CENTER.PLACE.G"));
+        container.getDashboard().addAutoRoutine("RED.LEAVE", new PathPlannerAuto("RED.LEAVE"));
+        container.getDashboard().addAutoRoutine("RED.PLACE.E", new PathPlannerAuto("RED.PLACE.E"));
     }
-    
+
     public static void addNamedCommands(RalphContainer container)
     {
-        NamedCommands.registerCommand("GoToL4Goal", container.getSuperstructure().getGoToGoalCommand(SuperstructureGoal.L4));
-        NamedCommands.registerCommand("GoToL3Goal", container.getSuperstructure().getGoToGoalCommand(SuperstructureGoal.L3));
-        NamedCommands.registerCommand("GoToL2Goal", container.getSuperstructure().getGoToGoalCommand(SuperstructureGoal.L2));
-        NamedCommands.registerCommand("GoToL1Goal", container.getSuperstructure().getGoToGoalCommand(SuperstructureGoal.L1));
+        NamedCommands.registerCommand("GoToL4Goal",
+                container.getSuperstructure().getGoToGoalCommand(SuperstructureGoal.L4));
+        NamedCommands.registerCommand("GoToL3Goal",
+                container.getSuperstructure().getGoToGoalCommand(SuperstructureGoal.L3));
+        NamedCommands.registerCommand("GoToL2Goal",
+                container.getSuperstructure().getGoToGoalCommand(SuperstructureGoal.L2));
+        NamedCommands.registerCommand("GoToL1Goal",
+                container.getSuperstructure().getGoToGoalCommand(SuperstructureGoal.L1));
         NamedCommands.registerCommand("GoToIntakeGoal",
-        container.getSuperstructure().getGoToGoalCommand(SuperstructureGoal.CORAL_STATION));
+                container.getSuperstructure().getGoToGoalCommand(SuperstructureGoal.CORAL_STATION));
         NamedCommands.registerCommand("Intake", container.getRollers().getCoralIntakeCommand(true));
         NamedCommands.registerCommand("Outtake", container.getRollers().getOuttakeCommand());
     }
