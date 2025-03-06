7cb3c32f6b6a6284540f079b019cc0f4ab29b836
Paradox0100 <milo.saucier@gmail.com>
2025-03-05 20:31:23 -0500

    Command class for Post Alignment With Xavier and new netwrok table requirment

---
+++ src/main/java/frc/robot/commands/AlignRobotPost.java
@@ -0,0 +1,71 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.ralph.RalphContainer;
+import static edu.wpi.first.units.Units.Meters;
+
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.units.measure.Distance;
+
+public class AlignRobotPost extends Command {
+    private Distance PostOffset;
+    private Distance PostDistance;
+    private boolean PostExist;
+
+    private double kp = 0.0;
+    private double ki = 0.0;
+    private double kd = 0.0;
+
+    private final PIDController xPidController;
+
+    private final RalphContainer ralphContainer;
+
+    public AlignRobotPost(RalphContainer ralphContainer) {
+        this.ralphContainer = ralphContainer;
+        xPidController = new PIDController(kp, ki, kd);
+    }
+
+    public void XOverideCalc() {
+        PPHolonomicDriveController.overrideXFeedback(() -> {
+            double xOffset = PostOffset.in(Meters);
+            return xPidController.calculate(xOffset);
+        });
+    }
+
+    @Override
+    public void execute() {
+        PostOffset = ralphContainer.getViewer().getPostOffset();
+        if (!ralphContainer.getViewer().getPostExist() && PostExist) {
+            PPHolonomicDriveController.clearXFeedbackOverride();
+            xPidController.reset();
+            PostExist = false;
+        } else if(ralphContainer.getViewer().getPostExist() && !PostExist) {
+            if (PostOffset.in(Meters) >= 0.1) {
+                XOverideCalc();
+            }
+            PostExist = true;
+        }
+    }
+
+    @Override
+    public void initialize() {
+        PPHolonomicDriveController.clearXFeedbackOverride();
+    }
+
+    @Override
+    public boolean isFinished() {
+        if (PostOffset.in(Meters) <= 0.1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        PPHolonomicDriveController.clearXFeedbackOverride();
+        xPidController.reset();
+    }
+}
--- src/main/java/frc/robot/subsystems/viewer/Viewer.java
+++ src/main/java/frc/robot/subsystems/viewer/Viewer.java
@@ -1,26 +1,27 @@
 package frc.robot.subsystems.viewer;
 
-import java.util.Set;
-
 import org.littletonrobotics.junction.Logger;
 
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.FieldConstants;
-import frc.robot.sebastian.constants.SebastianConstants;
-import frc.robot.subsystems.dashboard.DashboardIO;
+import edu.wpi.first.units.Units.*;
+import static edu.wpi.first.units.Units.Meters;
 
 public class Viewer extends SubsystemBase
 {
     private final ViewerIO io;
     private final ViewerIOInputsAutoLogged inputs;
     private final Alert viewerMissingAlert;
+
+    // find out the PID gains
+    private final PIDController xPidController = new PIDController(0.5, 0.5, 0.5);
 
     public Viewer(ViewerIO io)
     {
@@ -59,46 +60,5 @@ public class Viewer extends SubsystemBase
 
     public boolean getPostExist() {
         return inputs.postExist;
-    }
-
-    public Command alignWithPost()
-    {
-        return Commands.defer(() ->
-        {
-            Viewer viewer = getViewer();
-            Distance postOffset = viewer.getPostOffset();
-
-            boolean isRight = postOffset.in(Meters) > 0.0;
-            boolean isLeft = postOffset.in(Meters) < 0.0;
-
-            Pose2d currentPose = vision.getLatestPoseEstimate();
-            Pose2d newPose;
-
-            if (isRight)
-            {
-                newPose = new Pose2d(currentPose.getTranslation().plus(new Translation2d(postOffset.in(Meters), 0)),
-                        currentPose.getRotation());
-            } else if (isLeft)
-            {
-                newPose = new Pose2d(currentPose.getTranslation().plus(new Translation2d(-postOffset.abs(Meters), 0)),
-                        currentPose.getRotation());
-            } else
-            {
-                newPose = currentPose;
-            }
-
-            Pose2d backupPose = FieldConstants.getPostBackupPosition(newPose, Feet.of(1));
-
-            return getDrive()
-                    .driveToPose(backupPose, SebastianConstants.PathplannerConstants.MAX_VELOCITY,
-                            SebastianConstants.PathplannerConstants.MAX_ACCELERATION,
-                            SebastianConstants.PathplannerConstants.MAX_ANGULAR_VELOCITY,
-                            SebastianConstants.PathplannerConstants.MAX_ANGULAR_ACCELERATION)
-                    .alongWith(getSuperstructure().getGoToGoalCommand(getDashboard().getSuperstructureGoalForReef()))
-                    .andThen(() -> getDrive().driveToPose(newPose, SebastianConstants.PathplannerConstants.MAX_VELOCITY,
-                            SebastianConstants.PathplannerConstants.MAX_ACCELERATION,
-                            SebastianConstants.PathplannerConstants.MAX_ANGULAR_VELOCITY,
-                            SebastianConstants.PathplannerConstants.MAX_ANGULAR_ACCELERATION));
-        }, Set.of());
     }
 }
