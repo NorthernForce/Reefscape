ebaa7eeebaa414e6361568b1eaf61c5350a1cdf2
Kasey Nanovic <148725607+kcnan@users.noreply.github.com>
2025-03-10 19:43:37 -0400

    the build works on the ORANGE PI now

--- viewer/run.py
+++ viewer/run.py
@@ -2,13 +2,6 @@ from cscore import CameraServer
 import numpy as np
 import pyrealsense2 as rs
 import cv2
-# from ultralytics import YOLO
-
-# if len(sys.argv) != 2:
-#     print(f"usage: {sys.argv[0]} path/to/model.pt")
-#     sys.exit(1)
-
-# model = YOLO(sys.argv[1])
 
 w, h, fps = 640, 480, 30
 pipe = rs.pipeline()
@@ -21,24 +14,18 @@ config = rs.config()
 config.enable_stream(rs.stream.depth, w, h, rs.format.z16, fps)
 config.enable_stream(rs.stream.color, w, h, rs.format.rgb8, fps)
 
-align = rs.align(rs.stream.depth)
-profile = pipe.start(config)
-
-depth_scale = profile.get_device().first_depth_sensor().get_depth_scale()
-print(f"scale: {depth_scale}m")
+pipe.start(config)
 
 cs_video = CameraServer.putVideo("Video", w, h)
 cs_depth = CameraServer.putVideo("Depth", w, h)
 
 while True:
-    frames = align.process(pipe.wait_for_frames())
+    frames = pipe.wait_for_frames()
     depth = frames.get_depth_frame()
     color = frames.get_color_frame()
 
     color_array = np.asanyarray(color.get_data())
     dist_array = np.asanyarray(colorizer.colorize(depth).get_data())
-
-    print(dist_array.max(), "is max m")
     
     cs_video.putFrame(color_array)
     cs_depth.putFrame(cv2.convertScaleAbs(dist_array))
