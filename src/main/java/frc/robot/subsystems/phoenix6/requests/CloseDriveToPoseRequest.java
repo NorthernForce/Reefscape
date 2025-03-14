4025b9f8473dcfe625a6da95a4650c1cd38a2f19
cml1010101 <cml1010101@gmail.com>
2025-03-14 00:27:25 -0400

    Change tolerance

--- src/main/java/frc/robot/subsystems/phoenix6/requests/CloseDriveToPoseRequest.java
+++ src/main/java/frc/robot/subsystems/phoenix6/requests/CloseDriveToPoseRequest.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems.phoenix6.requests;
 
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Radians;
+
 import java.util.function.Supplier;
 
 import com.ctre.phoenix6.StatusCode;
@@ -9,6 +13,9 @@ import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearVelocity;
 
 import com.ctre.phoenix6.swerve.SwerveModule;
 import com.ctre.phoenix6.swerve.SwerveRequest;
@@ -19,23 +26,27 @@ public class CloseDriveToPoseRequest implements SwerveRequest
     private final Supplier<Pose2d> poseGetter;
     private final PIDController xPID;
     private final PIDController yPID;
+    private final LinearVelocity maxVelocity;
 
     public CloseDriveToPoseRequest(Pose2d pose, double tP, double tI, double tD, double rP, double rI, double rD,
+            LinearVelocity maxVelocity, Distance tolerance, Angle angleTolerance,
             Supplier<Pose2d> poseGetter)
     {
         this.xPID = new PIDController(tP, tI, tD);
         this.yPID = new PIDController(tP, tI, tD);
-        xPID.setTolerance(0.05);
-        yPID.setTolerance(0.05);
+        this.maxVelocity = maxVelocity;
+        xPID.setTolerance(tolerance.in(Meters));
+        yPID.setTolerance(tolerance.in(Meters));
         xPID.setSetpoint(pose.getX());
         yPID.setSetpoint(pose.getY());
         this.facingAngle = new FieldCentricFacingAngle();
         facingAngle.HeadingController.setPID(rP, rI, rD);
         facingAngle.HeadingController.enableContinuousInput(0, Math.PI * 2);
-        facingAngle.HeadingController.setTolerance(Math.toRadians(2));
+        facingAngle.HeadingController.setTolerance(angleTolerance.in(Radians));
         this.poseGetter = poseGetter;
         facingAngle.withTargetDirection(pose.getRotation());
         facingAngle.withDriveRequestType(DriveRequestType.Velocity);
+        facingAngle.withForwardPerspective(ForwardPerspectiveValue.BlueAlliance);
     }
 
     @Override
@@ -43,9 +54,8 @@ public class CloseDriveToPoseRequest implements SwerveRequest
     {
         double vx = xPID.calculate(poseGetter.get().getX());
         double vy = yPID.calculate(poseGetter.get().getY());
-        facingAngle.withVelocityX(MathUtil.clamp(vx, -1, 1));
-        facingAngle.withVelocityY(MathUtil.clamp(vy, -1, 1));
-        facingAngle.withForwardPerspective(ForwardPerspectiveValue.BlueAlliance);
+        facingAngle.withVelocityX(MathUtil.clamp(vx, -maxVelocity.in(MetersPerSecond), maxVelocity.in(MetersPerSecond)));
+        facingAngle.withVelocityY(MathUtil.clamp(vy, -maxVelocity.in(MetersPerSecond), maxVelocity.in(MetersPerSecond)));
         return facingAngle.apply(parameters, modulesToApply);
     }
 
