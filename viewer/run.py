2b4ff2ad4be6bfdb7b683c201be487c123e4f5e4
Kasey Nanovic <148725607+kcnan@users.noreply.github.com>
2025-03-19 20:06:33 -0400

    more changes (works better)

--- viewer/run.py
+++ viewer/run.py
@@ -1,4 +1,3 @@
-# from functools import reduce, partial
 from itertools import combinations
 from math import atan2, hypot, pi
 from cscore import CameraServer
@@ -36,10 +35,19 @@ cs_depth = CameraServer.putVideo("Depth", w//4, h//4) # low because we don't nee
 
 dist_thresh = 50
 frame = 0
+
+def find(parent, line):
+    if parent[line] != line:
+        return find(parent, parent[line])
+    return line
+
+def union(parent, line1, line2):
+    parent[find(parent, line2)] = find(parent, line1)
+
 while True:
     # test threshold values
-    frame = (frame + 1) % 500
-    dist_thresh = (frame/500) * 350
+    frame = (frame + 1) % 800
+    dist_thresh = (frame/800) * 250
 
     frames = pipe.wait_for_frames()
     frames = aligner.process(frames)
@@ -48,50 +56,51 @@ while True:
 
     color_img = cv2.cvtColor(np.asanyarray(color.get_data()), cv2.COLOR_RGB2BGR)
     dist_img = cv2.cvtColor(np.asanyarray(depth.get_data()), cv2.COLOR_RGB2GRAY)
-    dist_img = clahe.apply(dist_img)
     
-    ht, thresh_img = cv2.threshold(dist_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    ht, thresh_img = cv2.threshold(clahe.apply(dist_img), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     lt = ht * .9
     edges = cv2.Canny(dist_img, lt, ht)
+
     lines = cv2.HoughLinesP(edges, 1, np.pi/180, 50, None, 300, 500)
     lines = lines if lines is not None else []
+    # vertical lines ONLY (horizontal lines are the opps)
+    lines = list(filter(lambda x: abs(atan2(x[2]-x[0], x[3]-x[1])) < pi/16, map(lambda l: tuple(l[0]), lines)))
 
-    f_lines = []
+    # union-find algorithm time!
+    parent = dict((line, line) for line in lines)
+
+    for line1, line2 in combinations(lines, 2):
+        x1, y1, x2, y2 = line1
+        x3, y3, x4, y4 = line2
+        midx1, midy1 = ((x1+x2)/2, (y1+y2)/2)
+        midx2, midy2 = ((x3+x4)/2, (y3+y4)/2)
+        if abs(midx2-midx1) < dist_thresh:
+            union(parent, line2, line1)
+
+    groups = {}
     for line in lines:
-        x1, y1, x2, y2 = line[0]
-        if abs(atan2(x2-x1, y2-y1)) > pi/16: continue
-        f_lines.append(line[0])
+        rep = find(parent, line)
+        if rep not in groups.keys():
+            groups[rep] = list()
+        groups[rep].append(line)
 
-    f2_lines = []
-    merged_idxs = set()
-    for i in range(len(f_lines)):
-        x1, y1, x2, y2 = f_lines[i]
-        group = [f_lines[i]]
-        for j in range(i+1, len(f_lines)):
-            if j in merged_idxs:
-                continue
-            x3, y3, x4, y4 = f_lines[j]
-            midpx1, midpy1 = ((x1 + x2)/2, (y1 + y2)/2)
-            midpx2, midpy2 = ((x3 + x4)/2, (y3 + y4)/2)
-            if hypot(midpx1-midpx2, midpy1-midpy2) < dist_thresh:
-                merged_idxs.add(j)
-                group.append(f_lines[j])
-        
-        group = np.array(group)
-        x1avg = np.mean(group[:, 0])
-        y1avg = np.mean(group[:, 1])
-        x2avg = np.mean(group[:, 2])
-        y2avg = np.mean(group[:, 3])
-        f2_lines.append((x1avg, y1avg, x2avg, y2avg))
-    
+    avg_lines = []
+    for group in groups.values():
+        avg = np.uint(np.mean(group, axis=0))
+        avg_lines.append(avg)
+
     dbg_img = color_img.copy()
-    for x1, y1, x2, y2 in f2_lines:
-        dbg_img = cv2.line(dbg_img, (int(x1), int(y1)), (int(x2), int(y2)), (0,0,255), 3)
+    # dbg_img = cv2.addWeighted(color_img, .4, cv2.cvtColor(dist_img, cv2.COLOR_GRAY2BGR), .6, 0)
+    dbg_img = cv2.bitwise_or(dbg_img, cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR))
+    for x1, y1, x2, y2 in avg_lines:
+        dbg_img = cv2.line(dbg_img, (x1, y1), (x2, y2), (0, 255, 0), 12)
+        dbg_img = cv2.line(dbg_img, (int((x1+x2)/2), int((y1+y2)/2)), (int(w/2), int((y1+y2)/2)), (255, 0, 0), 3)
+    for x1, y1, x2, y2 in lines:
+        dbg_img = cv2.line(dbg_img, (x1, y1), (x2, y2), (0, 0, 255), 3)
 
     dbg_img = cv2.putText(
         dbg_img, f"{dist_thresh=:.1f}",
-        (20, 20), cv2.FONT_HERSHEY_COMPLEX, .5, (0, 0, 255), 1, cv2.LINE_AA)
-    
+        (20, 20), cv2.FONT_HERSHEY_COMPLEX, .5, (0, 0, 255), 1)
     # color = np.asanyarray(color.get_data())
     color_img = cv2.line(color_img, (w//2, 0), (w//2, h), (0, 255, 0), 10) # color when lined up good idea
     cs_video.putFrame(color_img)
